239e507207b6767f7a34ce916373c2bd57ce3418
sylvez90 <140185965+sylvez90@users.noreply.github.com>
2024-02-17 06:19:23 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
       <h5>Hello I'm</h5>
       <h1>Sylvester Williams</h1>
-      <h5 className='text-light'>Website Developer</h5>
+      <h5 className='text-light'>Frontend Developer</h5>
       <CTA />
       <HeaderSocials />
 
